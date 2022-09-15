484ba7afcefcbb808809f4b10bc8683113ddc34e
Michal Budzowski <michal.budzowski@sonarsource.com>
2022-09-15 14:20:44 +0200

    add files

---
+++ src/main/java/sonar/source/support/App.java
@@ -0,0 +1,16 @@
+package sonar.source.support;
+
+import java.util.List;
+import java.util.ArrayList;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	List list = new ArrayList();
+    }
+}
